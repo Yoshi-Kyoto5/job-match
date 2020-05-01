87e534c8282fd4fa83cc72d42063388df296d600
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-02 04:07:59 +0900

    add function of edit and update in user info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :img_name, :introduction, :job_name, :work_location, :work_time, :salary, :corona_support])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :sex, :img_name, :introduction, :job_name, :work_location, :work_time, :salary, :corona_support])
   end
 end
