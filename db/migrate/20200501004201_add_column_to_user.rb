b9f5eced032a1b645c0c081f0f439d309ce4d8b3
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-01 12:37:10 +0900

    remove null false of introduction column

--- db/migrate/20200501004201_add_column_to_user.rb
+++ db/migrate/20200501004201_add_column_to_user.rb
@@ -3,7 +3,7 @@ class AddColumnToUser < ActiveRecord::Migration[5.2]
     add_column :users, :name, :string, null: false, default: '', limit: 20
     add_column :users, :sex, :integer, null: false, default: 0
     add_column :users, :img_name, :string
-    add_column :users, :introduction, :text, null: false, limit: 300
+    add_column :users, :introduction, :text, limit: 300
     add_column :users, :job_name, :string, limit: 20
     add_column :users, :work_location, :string, limit: 10
     add_column :users, :work_time, :string, limit: 20
