05caf0f50b1f852871d6109a79c4daa0d19d3b78
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-01 12:49:54 +0900

    change miss code of protected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   
-  protedted
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :img_name, :introduction, :job_name, :work_location, :work_time, :salary, :corona_support])
