d22fc1d57e272dba471e8b051cd8e5c8a3314117
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-19 11:03:31 +0900

    comment out config action_cable in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_cable.allowed_request_origins = [ '18.180.146.214', /http:\/\/example.*/ ]
+  
+  # config.action_cable.allowed_request_origins = [ '18.180.146.214', /http:\/\/example.*/ ]
+  
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
