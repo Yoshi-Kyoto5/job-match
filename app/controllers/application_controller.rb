2a473f1f077cf32b03fdb920ab98804eadaa7258
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-20 07:27:19 +0900

    change code for csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, if: :devise_controller?
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
+  # protect_from_forgery with: :null_session
   
   protected
 
