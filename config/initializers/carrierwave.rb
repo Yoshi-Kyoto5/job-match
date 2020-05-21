56fe8b56cf37658ce070c4e4f01e70c2b4a13edd
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-21 14:14:24 +0900

    add config of fog and file for saving images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,21 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'job-match75'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/job-match75'
+    config.fog_directory  = 'job-match75'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/job-match75'
+  end
 end
